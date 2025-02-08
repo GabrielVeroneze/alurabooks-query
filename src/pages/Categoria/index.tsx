c27b13d30a0032af0934869163a2f7a51740166e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-07 22:16:12 -0300

    ✨ feat: adiciona estado de carregamento em Categoria

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -3,19 +3,33 @@ import { useParams } from 'react-router-dom'
 import { buscarCategoriaEspecifica } from '@/services/categorias'
 import { CategoriaDados } from '@/interfaces/CategoriaDados'
 import TituloPrincipal from '@/components/TituloPrincipal'
+import Loader from '@/components/Loader'
 import styles from './Categoria.module.scss'
 
 const Categoria = () => {
     const [categoria, setCategoria] = useState<CategoriaDados | null>(null)
+    const [estaCarregando, setEstaCarregando] = useState<boolean>(true)
     const params = useParams()
 
     useEffect(() => {
         if (params.slug) {
-            buscarCategoriaEspecifica(params.slug).then(dados =>
-                setCategoria(dados)
-            )
+            setEstaCarregando(true)
+
+            buscarCategoriaEspecifica(params.slug)
+                .then(dados => {
+                    setCategoria(dados)
+                    setEstaCarregando(false)
+                })
+                .catch(erro => {
+                    console.log(erro)
+                    setEstaCarregando(false)
+                })
         }
     }, [params])
+
+    if (estaCarregando) {
+        return <Loader />
+    }
 
     return (
         <section>

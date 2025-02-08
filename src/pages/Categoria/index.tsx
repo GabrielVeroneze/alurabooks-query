e3b583ace25707021979b7f5d6f9ecd3c1fdc4ee
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-08 17:10:54 -0300

    ♻️ refactor: substitui useState e useEffect por React Query

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -1,33 +1,19 @@
-import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
 import { buscarCategoriaEspecifica } from '@/services/categorias'
-import { CategoriaDados } from '@/interfaces/CategoriaDados'
 import TituloPrincipal from '@/components/TituloPrincipal'
 import Loader from '@/components/Loader'
 import styles from './Categoria.module.scss'
 
 const Categoria = () => {
-    const [categoria, setCategoria] = useState<CategoriaDados | null>(null)
-    const [estaCarregando, setEstaCarregando] = useState<boolean>(true)
     const params = useParams()
 
-    useEffect(() => {
-        if (params.slug) {
-            setEstaCarregando(true)
+    const { data: categoria, isLoading } = useQuery({
+        queryKey: ['categoria'],
+        queryFn: () => buscarCategoriaEspecifica(params.slug || ''),
+    })
 
-            buscarCategoriaEspecifica(params.slug)
-                .then(dados => {
-                    setCategoria(dados)
-                    setEstaCarregando(false)
-                })
-                .catch(erro => {
-                    console.log(erro)
-                    setEstaCarregando(false)
-                })
-        }
-    }, [params])
-
-    if (estaCarregando) {
+    if (isLoading) {
         return <Loader />
     }
 

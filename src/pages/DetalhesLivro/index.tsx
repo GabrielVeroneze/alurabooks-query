daa0f56696e19fab8f2a9ea91fffb68ae5562f5e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-02-11 23:29:13 -0300

    ✨ feat: cria página DetalhesLivro

---
+++ src/pages/DetalhesLivro/index.tsx
@@ -0,0 +1,60 @@
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { buscarLivroEspecifico } from '@/services/livros'
+import { buscarAutor } from '@/services/autores'
+import { formatarOpcoesCompra } from '@/utils/formatador-opcoes-compra'
+import Loader from '@/components/Loader'
+import TituloPrincipal from '@/components/TituloPrincipal'
+import Sobre from '@/components/Sobre'
+import LivroInfo from './LivroInfo'
+import FormatoLivro from './FormatoLivro'
+import ComprarLivro from './ComprarLivro'
+import styles from './DetalhesLivro.module.scss'
+
+const DetalhesLivro = () => {
+    const params = useParams()
+
+    const { data: livro, isLoading } = useQuery({
+        queryKey: ['livro', params.slug],
+        queryFn: () => buscarLivroEspecifico(params.slug || ''),
+    })
+
+    const { data: autor } = useQuery({
+        queryKey: ['autor', livro?.autor],
+        queryFn: () => buscarAutor(livro!.autor),
+    })
+
+    if (isLoading || !livro || !autor) {
+        return <Loader />
+    }
+
+    return (
+        <main>
+            <TituloPrincipal texto="Detalhes do livro" />
+            <section className={styles.container}>
+                <img
+                    className={styles.imagem}
+                    src={livro.imagemCapa}
+                    alt={livro.descricao}
+                />
+                <div className={styles.detalhes}>
+                    <LivroInfo
+                        titulo={livro.titulo}
+                        descricao={livro.descricao}
+                        autor={autor.nome}
+                    />
+                    <FormatoLivro
+                        opcoes={formatarOpcoesCompra(livro.opcoesCompra)}
+                    />
+                    <ComprarLivro />
+                </div>
+                <div className={styles.sobre}>
+                    <Sobre titulo="Sobre o Autor" texto={autor.sobre} />
+                    <Sobre titulo="Sobre o livro" texto={livro.sobre} />
+                </div>
+            </section>
+        </main>
+    )
+}
+
+export default DetalhesLivro
